2124035e4cb472b27fa3dca18a086144a7a4f2ec
MunawarShaik <munawarshaik8328@gmail.com>
2024-03-13 14:43:54 +0530

    completed Login page

--- src/App.js
+++ src/App.js
@@ -2,14 +2,22 @@ import React, { useState } from "react";
 import "./App.css";
 import Login from "./components/Login";
 import Homepage from "./components/Homepage";
-
+import { Route, Routes } from "react-router-dom";
+import Stack from "react-bootstrap/Stack";
 function App() {
   const [loggedIn, setLoggedIn] = useState(false);
-
   return (
-    <div className="App">
-      {!loggedIn ? <Login setLoggedIn={setLoggedIn} /> : <Homepage />}
-    </div>
+    <Stack className="App">
+      <Routes>
+        <Route
+          path="/login"
+          element={
+            !loggedIn ? <Login setLoggedIn={setLoggedIn} /> : <Homepage />
+          }
+        ></Route>
+        <Route path="/" element={loggedIn ? <Homepage /> : <Login />}></Route>
+      </Routes>
+    </Stack>
   );
 }
 
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import FormText from "react-bootstrap/FormText";
+import { useNavigate } from "react-router-dom";
 
-const Login = () => {
-  return <div>Login</div>;
-};
+function Login({ setLoggedIn }) {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (username === "dummyuser" && password === "123456") {
+      setLoggedIn(true);
+      navigate("/");
+    } else {
+      setError("Invalid credentials");
+    }
+  };
+
+  return (
+    <Container className="main d-flex justify-content-center align-items-center">
+      <Row>
+        <Col>
+          <h2 className="mb-6">Login</h2>
+          <Form onSubmit={handleSubmit} className="mt-2">
+            <div className="mb-3 d-flex">
+              <Form.Label className="mr-3">User Name</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter User Name"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
+            </div>
+
+            <div className="mb-3 d-flex justify-content-space-between  w-100">
+              <Form.Label className="mr-3">Enter Password</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+
+            <Button variant="primary" type="submit">
+              Login
+            </Button>
+            <FormText className="text-danger">
+              {" "}
+              {error && <p>{error}</p>}
+            </FormText>
+          </Form>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
 
 export default Login;
